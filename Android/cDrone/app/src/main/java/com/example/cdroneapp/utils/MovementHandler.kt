27dda171bb083825c81bc0fa248370b01e9ce5d2
ivargruneau <ivar.gruneau@gmail.com>
2024-03-04 03:01:04 +0100

    Created movementhandler

---
+++ Android/cDrone/app/src/main/java/com/example/cdroneapp/utils/MovementHandler.kt
@@ -0,0 +1,30 @@
+package com.example.cdroneapp.utils
+
+import dji.v5.manager.datacenter.MediaDataCenter
+import dji.v5.manager.datacenter.media.MediaFileListState
+
+class MovementHandler {
+
+    public fun init() {
+
+    }
+
+    public fun handleDirections(distance : Double, horizontalAngle : Double, verticalAngle : Double){
+
+    }
+
+    //Handles forward and backward movement
+    private fun move(){
+
+    }
+
+    //Handles yaw
+    private fun yaw() {
+
+    }
+
+    //Handles the cameras pitch
+    private fun pitch(){
+
+    }
+}
