921e12dd1970025fcad5665042f7809216136bd2
Warsay <warmah-0@student.ltu.se>
2024-03-05 13:53:19 +0100

    added control of rotation and movement

--- Android/cDrone/app/src/main/java/com/example/cdroneapp/MainActivity.kt
+++ Android/cDrone/app/src/main/java/com/example/cdroneapp/MainActivity.kt
@@ -32,9 +32,17 @@ class MainActivity : AppCompatActivity() {
     private lateinit var textView1: TextView
     private lateinit var textView2: TextView
     private lateinit var textView3: TextView
+    private lateinit var textView4: TextView
+    private lateinit var textView5: TextView
+    private lateinit var textView6: TextView
+
     private lateinit var button1 :Button
     private lateinit var button2 :Button
     private lateinit var button3 :Button
+    private lateinit var button4 :Button
+    private lateinit var button5 :Button
+    private lateinit var button6 :Button
+
 
     private lateinit var myApp : MyApplication
 
@@ -51,7 +59,14 @@ class MainActivity : AppCompatActivity() {
         textView2 = findViewById<TextView>(R.id.textView2)
         button3 = findViewById<Button>(R.id.button3)
         textView3 = findViewById<TextView>(R.id.textView3)
-
+        button4 = findViewById<Button>(R.id.button4)
+        textView4 = findViewById<TextView>(R.id.textView4)
+        button5 = findViewById<Button>(R.id.button5)
+        textView5 = findViewById<TextView>(R.id.textView5)
+        button6 = findViewById<Button>(R.id.button6)
+        textView6 = findViewById<TextView>(R.id.textView6)
+        button5.text = "TakeOff"
+        button6.text = "Landing"
 
         myApp = application as MyApplication
         button1.setOnClickListener {
@@ -63,8 +78,8 @@ class MainActivity : AppCompatActivity() {
         button2.setOnClickListener {
             //increment(textView2)
             //textView3.text = getHeight().toString()
-            virtualStickVM.listenRCStick()
-
+            //virtualStickVM.listenRCStick()
+            enableVS()
 
 
 
@@ -79,18 +94,49 @@ class MainActivity : AppCompatActivity() {
 
 
         }
+        button4.setOnClickListener {
+            //textView3.text = myApp.getStatusMessage()
+
+            //textView3.text = ttt.toString()
+
+
+
+
+        }
+
+        button5.setOnClickListener {
+            //textView3.text = myApp.getStatusMessage()
+            initTakeoff()
+            yawRight()
+            //textView3.text = ttt.toString()
+        }
+
+        button6.setOnClickListener {
+            //textView3.text = myApp.getStatusMessage()
+            initLanding()
+            //textView3.text = ttt.toString()
+
+
+
+
+        }
+
     }
     fun enableVS() {
         virtualStickVM.enableVirtualStick(object : CommonCallbacks.CompletionCallback {
             override fun onSuccess() {
-                textView3.text =("enableVirtualStick success.")
+                textView2.text =("enableVirtualStick success.")
             }
 
             override fun onFailure(error: IDJIError) {
-                textView3.text =("enableVirtualStick error,$error")
+                textView2.text =("enableVirtualStick error,$error")
             }
         })
+
     }
+
+
+
     fun getAircraftLocation() = FlightControllerKey.KeyAircraftLocation.create().get(LocationCoordinate2D(0.0, 0.0))
 
     private fun getHeight(): Double = (altitudeKey.get(0.0))
@@ -106,13 +152,55 @@ class MainActivity : AppCompatActivity() {
                 textView2.text =("start takeOff onFailure,$error")
             }
         })
+
+
+    }
+
+    private fun initLanding() {
+        basicAircraftControlVM.startLanding(object :
+            CommonCallbacks.CompletionCallbackWithParam<EmptyMsg> {
+            override fun onSuccess(t: EmptyMsg?) {
+                textView4.text = ("start Landing onSuccess.")
+            }
+
+            override fun onFailure(error: IDJIError) {
+                textView4.text = ("start Landing onFailure,$error")
+            }
+        })
+
+    }
+
+    fun moveForward() {
+        virtualStickVM.setRightPosition(0,30)
+
     }
 
 
+    fun moveBackward () {
+        virtualStickVM.setRightPosition(0,-30)
+    }
 
 
+    fun moveStop() {
+        //Stops all movement (forward and backward movement)
+        virtualStickVM.setRightPosition(0,0)
+    }
 
 
+    fun yawLeft () {
+        virtualStickVM.setLeftPosition(-30,0)
 
+    }
+
+
+    fun yawRight() {
+        virtualStickVM.setLeftPosition(30,0)
+    }
+
+
+    fun yawStop() {
+        //Stops all yaw (left and right yaw movement)
+        virtualStickVM.setLeftPosition(0,0)
+    }
 
 }
