5e8dfe43816191337ef4da6c1cde5e03d162c119
ivargruneau <ivar.gruneau@gmail.com>
2024-03-11 10:35:11 +0100

    Implementing the drawing of boxes

--- PedestrianDetection/Detector.py
+++ PedestrianDetection/Detector.py
@@ -1,6 +1,6 @@
 from ultralytics import YOLO
 import cv2
-
+import numpy as np
 
 # load yolov8 model
 
@@ -15,6 +15,7 @@ class Detector:
 # Assuming results.pred contains the predictions
 # And results.names contains the class names
     def calculate_distance_and_angle(self, x_Upper, y_Upper, x_Lower, y_Lower, image):
+
         image_width = image.shape[1]
         image_height = image.shape[0]
         base_distance = 10
@@ -30,6 +31,8 @@ class Detector:
         optimal_box_upper_y = (image_height -optimal_box_height )/2
         optimal_box_lower_x = (image_width +optimal_box_width )/2
         optimal_box_lower_y = (image_height +optimal_box_height )/2
+
+
         horizonal_angle = 0
         vertical_angle = 0
         distance = 0
@@ -59,13 +62,26 @@ class Detector:
             distance =  distance_in_image - base_distance
 
         return_params = distance, horizonal_angle, vertical_angle
+
+        #Draw the rectangles.
+        image_bounding_box = image.copy()
+        top_left_corner = (int(x_Upper), int(y_Upper)) 
+        bottom_right_corner = (int(x_Lower), int(y_Lower)) 
+        cv2.rectangle(image_bounding_box, top_left_corner, bottom_right_corner, (0, 0, 255), 8)
+        cv2.imwrite('./PedestrianDetection/testImages/bounding_box.jpg', image_bounding_box)
+
+        top_left_corner = (int(optimal_box_upper_x), int(optimal_box_upper_y)) 
+        bottom_right_corner = (int(optimal_box_lower_x), int(optimal_box_lower_y)) 
+        cv2.rectangle(image_bounding_box, top_left_corner, bottom_right_corner, (0, 255, 255), 8)
+        cv2.imwrite('./PedestrianDetection/testImages/optimal_box.jpg', image_bounding_box)
+
+
         return return_params
 
     #print(f'    x_Upper {x_Upper}. y_Upper {y_Upper} x_Lower { x_Lower}. y_Lower {y_Lower}. image_original_size {image_original_size}')
 
 
     def detect_on_image(self, image_path):
-        
         image = cv2.imread(image_path)
         detected = False
         distance = 0
@@ -116,7 +132,12 @@ def test_with_images(dir):
 
 #image_dir = './PedestrianDetection/testImages/'
 #test_with_images(image_dir)
-
+def draw_boxes():
+    image_dir = './PedestrianDetection/testImages/imgA.JPG'
+    detector = Detector()
+    detector.detect_on_image(image_dir)
+    print("Drawing comp")
+draw_boxes()
 #image = cv2.imread(image_dir+ image_names[0]+ ".JPG")
 #params = detect_on_image(image) 
 #print(f'returnd params: {params}')  
